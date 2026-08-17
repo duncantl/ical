7323bf85ec356016ba5dc642bd2fff3d3ef1564a
Duncan Temple Lang <duncan@r-project.org>
2026-08-16 18:18:01 -0700

    code to update the ical_parse in the ical namespace. From HotFix.

--- cal.R
+++ cal.R
@@ -1,4 +1,20 @@
-js = readLines("~/Personal/Calendar/cal.js");
+library(ical)
+js = readLines("cal.js")
 body(ical_parse)[[5]][[2]] = js
 
-b2 = as.data.frame(ical_parse(f))
+if(FALSE) {
+    ical_parse_df =
+        function(f)
+            as.data.frame(ical_parse(f))
+} else {
+
+    # or put the modified local ical_parse
+    ns = getNamespace("ical")
+    fn = "ical_parse"
+    unlockBinding(fn, ns)
+    environment(ical_parse) = ns
+    assign(fn, ical_parse, envir = ns)
+    lockBinding(fn, ns)
+    rm(ical_parse, js)
+
+}
